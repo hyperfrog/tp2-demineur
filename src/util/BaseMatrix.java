3bd046c0ac2be9c0d7999ac03d7f30ac08a9759b
lesage.christian@gmail.com <lesage.christian@gmail.com@b86b8adc-0d1b-0daa-23bc-2e70e55ed913>
2011-10-21 15:21:31 +0000

    Ajout d'une matrice de base

---
+++ src/util/BaseMatrix.java
@@ -0,0 +1,136 @@
+/**
+ * 
+ */
+package util;
+
+/**
+ * La classe BaseMatrix implémente une matrice d'éléments de type Object.
+ * 
+ * @author Alexandre Tremblay
+ * @author Christian Lesage
+ *
+ */
+
+public class BaseMatrix
+{
+	// Contient la matrice courante.
+	private Object[][] m;
+	
+	// Largeur de la matrice courante.
+	private int width;
+	
+	// Hauteur de la matrice courante.
+	private int height;
+	
+	/**
+	 * Construit une matrice de la largeur et la hauteur spécifiées
+	 * en ajoutant le nombre de mines passée en paramètre à l'intérieur de la matrice.
+	 * 
+	 * @param width
+	 * @param height
+	 * @param mineAmount
+	 */
+	public BaseMatrix(int width, int height)
+	{
+		
+		Object[][] newM = new Object[width][height];
+		
+		this.width = newM[0].length;
+		this.height = newM.length;
+		this.m = newM;
+		
+//		this.populate();
+	}
+
+	/**
+	 * Retourne l'élément de la matrice présent à la position (x, y) donnée.
+	 * 
+	 * @param x		Position en largeur.
+	 * @param y		Position en hauteur
+	 * @return		L'élément à la position donnée
+	 */
+	public Object getElement(int x, int y)
+	{
+		Object o = null;
+		
+		if ((x < this.getWidth() && x >= 0) && (y < this.getHeight() && y >= 0))
+		{
+			o = this.m[x][y];
+		}
+			
+		return o;
+	}
+	
+	/**
+	 * Remplace l'élément à la position (x, y) donnée par l'élément passé
+	 * en paramètre. Celui-ci ne doit pas être null et la position demandée
+	 * doit se trouver à l'intérieur de la matrice.
+	 * 
+	 * @param x Position en largeur
+	 * @param y	Position en hauteur
+	 * @param o	Nouvel élément
+	 * 
+	 * @return succeed Retourne true si l'opération s'est produite avec succès, sinon false
+	 */
+	public boolean setElement(int x, int y, Object o)
+	{
+		boolean succeed = false;
+		
+		if (o != null && (x < this.getWidth() && x >= 0) && (y < this.getHeight() && y >= 0))
+		{
+			this.m[x][y] = o;
+			succeed = true;
+		}
+		
+		return succeed;
+	}
+	
+	/**
+	 * Retourne une représentation visuelle de la matrice.
+	 * 
+	 * @return s	Représentation visuelle de la matrice.
+	 */
+	public String toString()
+	{
+		String s = new String("");
+		
+		for (int i = 0; i < this.getHeight(); i++)
+		{
+			s += "[ ";
+			
+			for (int j = 0; j < this.getWidth(); j++)
+			{
+				s += this.getElement(j, i).toString();
+				
+				if (j < this.getWidth() - 1)
+				{
+					s += ", ";
+				}
+			}
+			
+			s += " ]\n";
+		}
+		
+		return s;
+	}
+	
+	/**
+	 * Retourne la largeur de la matrice.
+	 * 
+	 * @return		La largeur de la matrice.
+	 */
+	public int getWidth()
+	{
+		return this.width;
+	}
+	
+	/**
+	 * Retourne la hauteur de la matrice.
+	 * 
+	 * @return		La hauteur de la matrice.
+	 */
+	public int getHeight()
+	{
+		return this.height;
+	}
+}
