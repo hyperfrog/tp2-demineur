df18ffa19c23342ea3deda9457133b61f7858fb7
lesage.christian@gmail.com <lesage.christian@gmail.com@b86b8adc-0d1b-0daa-23bc-2e70e55ed913>
2011-10-21 15:22:08 +0000

    Remodélisation de la matrice de jeu (ajout d'une matrice de base)

--- trunk/src/appDemineur/model/Matrix.java
+++ trunk/src/appDemineur/model/Matrix.java
@@ -2,40 +2,25 @@ package appDemineur.model;
 
 import java.util.ArrayList;
 import java.util.Random;
+import util.BaseMatrix;
 
 /**
- * La classe Matrix ...
+ * La classe Matrix implémente la matrice du jeu du démineur.
+ * Elle est composée de cellules du type Cell.
  * 
  * @author Alexandre Tremblay
  * @author Christian Lesage
+ * 
+ * @See appDemineur.model.Cell
  *
  */
 
-public class Matrix
+public class Matrix extends BaseMatrix
 {
-	// Contient la matrice courante.
-	private Cell[][] m;
-	
-	// Largeur de la matrice courante.
-	private int width;
-	
-	// Hauteur de la matrice courante.
-	private int height;
-	
 	/**
-	 * Construit une nouvelle matrice de la largeur et la hauteur passée en paramètre.
-	 * 
-	 * @param width Largeur de la matrice
-	 * @param height Hauteur de la matrice
-	 */
-	public Matrix(int width, int height)
-	{
-		this(width, height, 40);
-	}
-	
-	/**
-	 * Construit une nouvelle matrice de la largeur et la hauteur passée en paramètre
-	 * en ajoutant le nombre de mines passée en paramètre à l'intérieur de la matrice.
+	 * Construit une matrice de la largeur et la hauteur spécifiées
+	 * et y ajoute le nombre de mines passé en paramètre dans des cellules
+	 * choisies au hasard.
 	 * 
 	 * @param width
 	 * @param height
@@ -43,7 +28,7 @@ public class Matrix
 	 */
 	public Matrix(int width, int height, int mineAmount)
 	{
-		this.set(new Cell[width][height]);
+		super(width, height);
 		this.populate();
 		this.addMines(mineAmount);
 	}
@@ -60,19 +45,21 @@ public class Matrix
 		}
 	}
 	
+	// Remplit la matrice avec un nombre «amount» de mines
+	// et détermine, pour chaque cellule, le nombre de mines adjacentes
 	private void addMines(int amount)
 	{
 		// «Chapeau» duquel on va tirer des numéros de cellule 
 		ArrayList<Integer> allCells = new ArrayList<Integer>();
 		
 		// Ajoute chaque numéro de cellule dans le chapeau
-		for(int i = 0; i < this.width * this.height; i++)
+		for (int i = 0; i < this.getWidth() * this.getHeight(); i++)
 		{
 			allCells.add(i);
 		}
 		
 		// Tire un nombre «amount» de cellules du chapeau
-		for(int i = 0; i < amount; i++)
+		for (int i = 0; i < amount; i++)
 		{
 			Random rand = new Random();
 
@@ -80,49 +67,34 @@ public class Matrix
 			
 			int cellNum = allCells.remove(cellIdx);
 			
-			int x = cellNum % this.width;
-			int y = cellNum / this.height;
+			int x = cellNum % this.getWidth();
+			int y = cellNum / this.getHeight();
 						
-			// On place une mine dans cette cellule
-			this.getElement(x, y).setAsMine(true);
-			
-			// On incrémente le compteur de mines adjacentes de chacune des mines adjacentes
+			// Parcourt les 9 cellules incluant la cellule courante et les cellules adjacentes
 			for (int r = y - 1 ; r <= y + 1; r++)
 			{
 				for (int c = x - 1 ; c <= x + 1; c++)
 				{
-					// Si ce n'est pas la cellule courante
-					if (!(r == y && c == x))
+					Cell cell = this.getElement(c, r);
+					// S'il y a vraiment une cellule à ces coordonnées
+					if (cell != null)
 					{
-						Cell cell = this.getElement(c, r);
-						// S'il y a vraiment une cellule à ces coordonnées
-						if (cell != null)
+						// Si ce n'est pas la cellule courante
+						if (!(r == y && c == x))
 						{
 							// Incrémente le compteur de mines adjacentes
 							cell.setAdjacentMines(cell.getAdjacentMines() + 1);
+						}
+						else // On place une mine dans cette cellule
+						{
+							cell.setAsMine(true);
 						}
 					}
 				}
 			}
 		}
 	}
-	
-	/**
-	 * Remplace la matrice courante par une nouvelle matrice passée en paramètre.
-	 * Celle-ci ne doit pas être null.
-	 * 
-	 * @param newM		Nouvelle matrice
-	 */
-	public void set(Cell[][] newM)
-	{
-		if (newM != null && newM.length != 0)
-		{
-			this.setWidth(newM[0].length);
-			this.setHeight(newM.length);
-			this.m = newM;
-		}
-	}
-	
+
 	/**
 	 * Retourne l'élément de la matrice présent à la position (x, y) donnée.
 	 * 
@@ -130,104 +102,11 @@ public class Matrix
 	 * @param y		Position en hauteur
 	 * @return		L'élément à la position donnée
 	 */
+	@Override
 	public Cell getElement(int x, int y)
 	{
-		Cell s = null;
+		Object o = super.getElement(x, y);
 		
-		if ((x < this.getWidth() && x >= 0) && (y < this.getHeight() && y >= 0))
-			s = this.m[x][y];
-			
-		return s;
-	}
-	
-	/**
-	 * Remplace l'élément à la position (x, y) donnée par l'élément passée
-	 * en paramètre. Celui-ci ne doit pas être null et la position demandée
-	 * doit se trouver à l'intérieur de la matrice.
-	 * 
-	 * @param x Position en largeur
-	 * @param y	Position en hauteur
-	 * @param o	Nouvel élément
-	 * 
-	 * @return succeed Retourne true si l'opération s'est produite avec succès, sinon false
-	 */
-	public boolean setElement(int x, int y, Cell s)
-	{
-		boolean succeed = false;
-		
-		if (s != null && (x < this.getWidth() && x >= 0) && (y < this.getHeight() && y >= 0))
-		{
-			this.m[x][y] = s;
-			succeed = true;
-		}
-		
-		return succeed;
-	}
-	
-	/**
-	 * Retourne une représentation visuelle de la matrice.
-	 * 
-	 * @return s	Représentation visuelle de la matrice.
-	 */
-	public String toString()
-	{
-		String s = new String("");
-		
-		for (int i = 0; i < this.getHeight(); i++)
-		{
-			s += "[ ";
-			
-			for (int j = 0; j < this.getWidth(); j++)
-			{
-				s += this.getElement(j, i).toString();
-				
-				if (j < this.getWidth() - 1)
-					s += ", ";
-			}
-			
-			s += " ]\n";
-		}
-		
-		return s;
-	}
-	
-	/**
-	 * Retourne la largeur de la matrice.
-	 * 
-	 * @return		La largeur de la matrice.
-	 */
-	public int getWidth()
-	{
-		return this.width;
-	}
-	
-	/**
-	 * Remplace la largeur courante par celle passée en paramètre.
-	 * 
-	 * @param w		La nouvelle largeur de la matrice.
-	 */
-	public void setWidth(int w)
-	{
-		this.width = w;
-	}
-	
-	/**
-	 * Retourne la hauteur de la matrice.
-	 * 
-	 * @return		La hauteur de la matrice.
-	 */
-	public int getHeight()
-	{
-		return this.height;
-	}
-	
-	/**
-	 * Remplace la hauteur courante par celle passée en paramètre.
-	 * 
-	 * @param h		La nouvelle hauteur de la matrice.
-	 */
-	public void setHeight(int h)
-	{
-		this.height = h;
+		return o instanceof Cell ? (Cell)o : null;
 	}
 }
