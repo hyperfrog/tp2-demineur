64056d9d74b829fc452c188b426ef007df8ef061
alx.tremblay@gmail.com <alx.tremblay@gmail.com@b86b8adc-0d1b-0daa-23bc-2e70e55ed913>
2011-10-22 23:23:17 +0000

    Maintenant possible avec le clic droit d'alterner entre les états FLAGGED, DUBIOUS et HIDDEN.

--- trunk/src/appDemineur/form/AppFrame.java
+++ trunk/src/appDemineur/form/AppFrame.java
@@ -22,10 +22,10 @@ public class AppFrame extends JFrame implements ComponentListener
 	private static final Dimension INIT_SIZE = new Dimension(624, 683);
 	
 	// Largeur minimale de la fenêtre
-	private static final int MIN_WIDTH = 400;
+	private static final int MIN_WIDTH = 350;
 
 	// Hauteur minimale de la fenêtre
-	private static final int MIN_HEIGHT = 300;
+	private static final int MIN_HEIGHT = 410;
 	
 	// Titre de la fenêtre
 	private static final String INIT_TITLE = "Démineur par Alexandre Tremblay et Christian Lesage";
