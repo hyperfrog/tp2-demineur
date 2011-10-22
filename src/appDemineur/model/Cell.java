f815eb1d5954ca8e8b039231822beebe7c2f1274
lesage.christian@gmail.com <lesage.christian@gmail.com@b86b8adc-0d1b-0daa-23bc-2e70e55ed913>
2011-10-22 03:16:11 +0000

    Simplifications mineures

--- src/appDemineur/model/Cell.java
+++ src/appDemineur/model/Cell.java
@@ -21,31 +21,35 @@ public class Cell
 	
 	public void redraw(Graphics g, float size)
 	{
+		int chrX = Math.round(size * 0.35f);
+		int chrY = Math.round(size * 0.75f);
+		int fontSize = Math.round(size * 0.60f);
+		int cellSize = Math.round(size);
+		
 		if (g != null)
 		{
 			if (!this.getState().equals(CellState.SHOWN) && !this.isMine())
 			{
 				g.setColor(Color.GRAY);
-//				g.fill3DRect(0, 0, Math.round(size), Math.round(size), true);
-				g.fillRect(0, 0, Math.round(size), Math.round(size));
+				g.fillRect(0, 0, cellSize, cellSize);
 				
 				g.setColor(Color.WHITE);
-				g.setFont(new Font(null, Font.BOLD, Math.round((12 * size) / 20)));
+				g.setFont(new Font(null, Font.BOLD, fontSize));
 				
 				if (this.getState().equals(CellState.DUBIOUS))
 				{
-					g.drawString("?", (int) ((7 * size) / 20), (int) ((15 * size) / 20));
+					g.drawString("?", chrX, chrY);
 				}
 				else if (this.getState().equals(CellState.FLAGGED))
 				{
-					g.drawString("F", (int) ((7 * size) / 20), (int) ((15 * size) / 20));
+					g.drawString("F", chrX, chrY);
 				}
 			}
 			else if (this.getState().equals(CellState.SHOWN) && !this.isMine())
 			{
 				g.setColor(Color.WHITE);
-				g.fill3DRect(0, 0, Math.round(size), Math.round(size), true);
-				
+				g.fillRect(0, 0, cellSize, cellSize);
+
 				switch (this.getAdjacentMines())
 				{
 					case 1: g.setColor(new Color(106, 133, 165)); break;
@@ -57,14 +61,14 @@ public class Cell
 					case 7: g.setColor(new Color(171, 31,  26 )); break;
 					case 8: g.setColor(new Color(128, 0,   0  )); break;
 				}
-				
-				g.setFont(new Font(null, Font.BOLD, Math.round((12 * size) / 20)));
-				g.drawString("" + this.getAdjacentMines(), (int) ((7 * size) / 20), (int) ((15 * size) / 20));
+
+				g.setFont(new Font(null, Font.BOLD, fontSize));
+				g.drawString("" + this.getAdjacentMines(), chrX, chrY);
 			}
-			else if (this.getState().equals(CellState.HIDDEN) && this.isMine())
+			else if (this.getState().equals(CellState.HIDDEN) && this.isMine()) 
 			{
 				g.setColor(Color.BLACK);
-				g.fill3DRect(0, 0, Math.round(size), Math.round(size), true);
+				g.fillRect(0, 0, cellSize, cellSize);
 				g.setColor(Color.RED);
 				// TODO : Corriger le cercle
 				g.fillOval(5, 5, 10, 10);
